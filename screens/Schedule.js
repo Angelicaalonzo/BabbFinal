b8707c9a94c1ca0f258930373bd91d1fe8039dfa
Mara Kinoff <maranicholemayfield@gmail.com>
2019-05-02 19:09:47 -0500

    adding basic calendar functionality

--- screens/Schedule.js
+++ screens/Schedule.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 export default class Schedule extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button
-          title="Home"
-          onPress={() =>
-            this.props.navigation.navigate('Home')
-          }
+        <Calendar
+          minDate={'2019-01-01'}
+          maxDate={'2019-12-31'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => {console.log('selected day', day)}}
+          // Handler which gets executed on day long press. Default = undefined
+          onDayLongPress={(day) => {console.log('selected day', day)}}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'MMMM yyyy'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => {console.log('month changed', month)}}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={1}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+          onPressArrowLeft={substractMonth => substractMonth()}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go next month
+          onPressArrowRight={addMonth => addMonth()}
+          markedDates={{
+            '2019-11-06': {marked: true},
+            '2019-11-07': {marked: true},
+            '2019-11-08': {marked: true},
+            '2019-11-09': {marked: true}
+          }}
         />
-        <Text> This is the schedule page. </Text>
       </View>
     );
   }
